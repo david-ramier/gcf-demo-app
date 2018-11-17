eff567b642d84bc7da355283d6037240eb6b1759
Marco Maccio <marco.maccio@marmac.name>
2018-11-17 16:47:30 +0000

    gcf-demo-app-1 Integrate with Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey:             'AIzaSyDvSzdeHdoyDIfhNb5BizqTUsqmsVgiMfM',
+    authDomain:         'name-marmac-gcf-demo.firebaseapp.com',
+    databaseURL:        'https://name-marmac-gcf-demo.firebaseio.com',
+    projectId:          'name-marmac-gcf-demo',
+    storageBucket:      'name-marmac-gcf-demo.appspot.com',
+    messagingSenderId:  '52176016320'
+  }
 };
