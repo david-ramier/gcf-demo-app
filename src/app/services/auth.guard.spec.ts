a91978868812b99fe957746abe97c98a9aa6b926
Marco Maccio <marco.maccio@marmac.name>
2018-11-22 08:30:46 +0000

    gcf-demo-app-19 SignUp Added service to be inject but need firebase stub

--- src/app/services/auth.guard.spec.ts
+++ src/app/services/auth.guard.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed, async, inject } from '@angular/core/testing';
 
 import { AuthGuard } from './auth.guard';
+import { AuthenticationService } from './authentication.service';
+import {AppUser} from '../models/app-user';
+import {AngularFireAuth} from '@angular/fire/auth';
+import {AngularFirestore, AngularFirestoreDocument} from '@angular/fire/firestore';
 
 describe('AuthGuard', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AuthGuard]
+      providers: [AuthGuard, AuthenticationService, AngularFireAuth, AngularFirestore]
     });
   });
 
